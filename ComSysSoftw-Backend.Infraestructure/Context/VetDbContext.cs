e48194a8fbfab7c73836195e1f6df37873d82773
Rodrigo Pozo <rodrigo.pozo25@gmail.com>
2023-06-23 09:56:43 -0500

    add hosting DB

--- ComSysSoftw-Backend.Infraestructure/Context/VetDbContext.cs
+++ ComSysSoftw-Backend.Infraestructure/Context/VetDbContext.cs
@@ -24,7 +24,7 @@ public class VetDbContext : DbContext
         if (!optionsBuilder.IsConfigured)
         {
             var serverVersion = new MySqlServerVersion(new Version(8, 0, 29));
-            optionsBuilder.UseMySql("Server=localhost,3306;Uid=root;Pwd= *** ;Database=VetDB;", serverVersion);
+            optionsBuilder.UseMySql("Server=sql10.freemysqlhosting.net,3306;Uid=sql10628214;Pwd=ISd6XkqXWs;Database=sql10628214;", serverVersion);
         }
     }
 
